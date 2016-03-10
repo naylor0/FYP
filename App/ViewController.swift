89c7ea858bb64e7299ff3be3ffa90d410ca265e3
naylor0 <mark.naylor@mydit.ie>
2016-03-10 14:49:04 +0000

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Mark Naylor on 10/03/2016.
+//  Copyright © 2016 Mark Naylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
